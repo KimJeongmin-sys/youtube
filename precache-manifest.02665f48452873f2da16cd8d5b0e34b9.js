1aa7533f8fbbbe39b61f20d20b856ec0bfe73314
KimJeongmin-sys <hikjmjuly23@gmail.com>
2020-10-19 01:35:28 +0900

    Updates

---
+++ precache-manifest.02665f48452873f2da16cd8d5b0e34b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8db0effa94c990964e",
+    "url": "/youtube/static/js/runtime~main.a6ce4185.js"
+  },
+  {
+    "revision": "ba52dbccc74db1e27bdd",
+    "url": "/youtube/static/js/main.4996cb16.chunk.js"
+  },
+  {
+    "revision": "2be40580f3d26cb22156",
+    "url": "/youtube/static/js/2.03b949a3.chunk.js"
+  },
+  {
+    "revision": "ba52dbccc74db1e27bdd",
+    "url": "/youtube/static/css/main.f26408f8.chunk.css"
+  },
+  {
+    "revision": "3dbe2171bdffbc22c5df016f48230a26",
+    "url": "/youtube/index.html"
+  }
+];
